6603d68912221f2630115b4ea5c9956e11c99185
eng-david <scuba-potions-0a@icloud.com>
2022-07-22 20:25:39 -0300

    commit inicial

---
+++ src/main/java/br/com/alura/linguagens/api/Language.java
@@ -0,0 +1,39 @@
+package br.com.alura.linguagens.api;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
+public class Language {
+
+    @Id
+    private String id;
+    private String title;
+    private String image;
+    private int ranking;
+      
+    public Language(){
+
+    }
+
+    public Language(String title, String image, int ranking) {
+        this.title = title;
+        this.image = image;
+        this.ranking = ranking;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public String getImage() {
+        return image;
+    }
+    public int getRanking() {
+        return ranking;
+    }
+    public String getId() {
+        return id;
+    }
+    
+    
+}
